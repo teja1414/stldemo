161e2253c5f1829b852a587eeb611a792fee4948
teja1414 <teja141414@gmail.com>
2022-08-13 08:49:04 +0530

    sorted in desc

--- main.cpp
+++ main.cpp
@@ -15,5 +15,11 @@ int main()
 
     cout<<"sum of vector elements: "<<sum<<endl;
     cout<<endl;
+
+    /////////sort elements in descending order
+    sort(v1.begin(),v1.end(),greater<int>());
+    cout<<"v1:{";
+    for_each(begin(v1),end(v1),[](const auto& x){cout<<x<<" ";});
+    cout<<"}"<<endl;
     return 0;
 }
